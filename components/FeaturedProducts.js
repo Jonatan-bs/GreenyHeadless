bbf5b1866d0b8d4cec2ef9374da02fa2631cf2e1
Jonatan Shoshan <jonatan-bs@live.dk>
2021-02-05 17:10:59 +0100

    Added frontpage products

---
+++ components/FeaturedProducts.js
@@ -0,0 +1,24 @@
+import SingleProduct from "./partials/SingleProduct"; 
+const FeaturedProducts = (props) => (
+    
+    <div className="product-list center">
+        <div className="content col-4 gap-5">
+            { props.products.map( (product,i) => {
+                // Check if Show on frontpage is true
+                
+                if (product.meta_data.some(function(p){return p['key'] === "show_on_frontpage" && p['value'] })){
+                    return <SingleProduct key={product.id} product={product}/>
+                }
+            })  }
+             
+        </div>
+
+        <div className="flexcenter">
+            <Link href="/shop">
+                <a className="page-link" >Shop</a>
+            </Link>
+        </div>
+    </div>
+);
+
+export default FeaturedProducts;
---
+++ components/partials/SingleProduct.js
@@ -0,0 +1,38 @@
+import Link from 'next/link'
+
+const SingleProduct = (props) => (
+
+
+    <div id={`product-${props.product.id}`} className={`product animate slideIn sequence ${props.loadmore ? ' activated ' : ''}`}> 
+        
+        <Link href={props.product.permalink}>
+            <a>
+                <img  className="product-image" src={props.product.images[0].src}  alt={props.product.images[0].alt}/>
+                
+                { props.product.stock_status !== "instock" ? (<div className="info outofstock">Out of stock</div>):""}
+                { props.product.on_sale ? (<div className="info sale">Sale!</div>):"" }      
+
+            </a>
+        </Link>
+        <Link href={props.product.permalink}>
+            <a >
+                <p className="display-md color-dark  title">{props.product.name}</p>
+            </a>
+        </Link>
+
+        <p className="color-medium excerpt">{props.product.short_description.replace(/(<([^>]+)>)/gi, "")}</p>
+        <div className="flex">   
+            { props.product.purchasable && props.product.stock_status === "instock" ? (
+        
+                <p className={`price ${props.product.on_sale ? onsale : ""}`}>
+                    { props.product.type !== 'variable' ? (
+                        props.product.on_sale ? <span className="sales-price">{props.product.regular_price}</span> + props.product.price : props.product.price 
+                    ) : "" }
+                </p>
+            ) : ""}
+                
+        </div>
+    </div>
+);
+
+export default SingleProduct;
